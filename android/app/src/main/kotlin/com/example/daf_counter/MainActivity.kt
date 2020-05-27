12578c4f7191dcadb90a1e3c06ac6f805b9ad240
chaimgross <chaimgross1026@gmail.com>
2020-05-27 22:07:26 +0300

    Broken : use for notification testing only

--- android/app/src/main/kotlin/com/example/daf_counter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/daf_counter/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.zektec.daf_plus_plus
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.os.Bundle
+import android.content.ContentResolver
+import android.content.Context
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+class MainActivity : FlutterActivity() {
+    override protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+//        MethodChannel(getFlutterView(), "crossingthestreams.io/resourceResolver").setMethodCallHandler(
+//                object : MethodCallHandler() {
+//                    fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+//                        if ("drawableToUri" == call.method) {
+//                            val resourceId: Int = this@MainActivity.getResources().getIdentifier(call.arguments as String, "drawable", this@MainActivity.getPackageName())
+//                            val uriString = resourceToUriString(this@MainActivity.getApplicationContext(), resourceId)
+//                            result.success(uriString)
+//                        }
+//                    }
+//                })
+    }
+
+    companion object {
+        private fun resourceToUriString(context: Context, resId: Int): String {
+            return (ContentResolver.SCHEME_ANDROID_RESOURCE
+                    + "://"
+                    + context.getResources().getResourcePackageName(resId)
+                    + "/"
+                    + context.getResources().getResourceTypeName(resId)
+                    + "/"
+                    + context.getResources().getResourceEntryName(resId))
+        }
     }
 }
--- lib/pages/onboarding/welcome.dart
+++ lib/pages/onboarding/welcome.dart
@@ -1,3 +1,9 @@
+import 'package:daf_plus_plus/actions/progress.dart';
+import 'package:daf_plus_plus/models/daf.dart';
+import 'package:daf_plus_plus/models/progress.dart';
+import 'package:daf_plus_plus/services/hive/index.dart';
+import 'package:daf_plus_plus/stores/dafsDates.dart';
+import 'package:daf_plus_plus/utils/dateConverter.dart';
 import 'package:daf_plus_plus/widgets/core/spacer.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +11,8 @@ import 'package:daf_plus_plus/consts/consts.dart';
 import 'package:daf_plus_plus/pages/onboarding/onboarding1.dart';
 import 'package:daf_plus_plus/utils/localization.dart';
 import 'package:daf_plus_plus/widgets/core/button.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
 
 class WelcomeOnboardingPage extends StatefulWidget {
   @override
@@ -13,6 +21,8 @@ class WelcomeOnboardingPage extends StatefulWidget {
 
 class _WelcomeOnboardingPageState extends State<WelcomeOnboardingPage> {
   List<String> _listOfLanguages = [""];
+  int count = 0;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   void _changeLanguage(BuildContext context, String language) async {
     await localizationUtil.setPreferredLanguage(language);
@@ -26,7 +36,7 @@ class _WelcomeOnboardingPageState extends State<WelcomeOnboardingPage> {
 
   void _getLanguages() {
     List<String> listOfLanguages =
-        Consts.LOCALES.map((Locale language) => language.languageCode).toList();
+    Consts.LOCALES.map((Locale language) => language.languageCode).toList();
     setState(() {
       _listOfLanguages = listOfLanguages;
     });
@@ -36,6 +46,64 @@ class _WelcomeOnboardingPageState extends State<WelcomeOnboardingPage> {
   void initState() {
     super.initState();
     _getLanguages();
+    final initializationSettingsAndroid =
+    AndroidInitializationSettings('ic_launcher');
+    final initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    final initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    final categories = [
+      NotificationCategory(
+        "MY_NOTIFICATION_CATEGORY",
+        [
+          NotificationAction("Mark as Read", "ACTION_YAY"),
+          NotificationAction("Not Yet", "ACTION_NAY"),
+        ],
+      ),
+    ];
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: onSelectNotification,
+      onSelectNotificationAction: onSelectNotificationAction,
+      categories: categories,
+    );
+  }
+
+  Future<void> _showNotificationWithActions() async {
+    var androidDetails = AndroidNotificationDetails(
+      'notification with actions channel id',
+      'notification with actions channel name',
+      'notification with actions channel description',
+    );
+    var iosDetails = IOSNotificationDetails();
+    var details = NotificationDetails(androidDetails, iosDetails);
+    var scheduledNotificationDateTime =  DateTime.now().add(Duration(minutes: 1, seconds: 05));
+    var time = Time(scheduledNotificationDateTime.hour, scheduledNotificationDateTime.minute + 1, 0);
+    count = count + 1;
+    await flutterLocalNotificationsPlugin.schedule(
+        count,
+        'Did the Daf?',
+        'Did you learn today\'s daf?',
+        scheduledNotificationDateTime,
+        details,
+        categoryIdentifier: "MY_NOTIFICATION_CATEGORY",
+        payload: '');
+  }
+
+
+  Future<void> onSelectNotificationAction(NotificationActionData data) async {
+    DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm:ss");
+    if (data.actionIdentifier == "ACTION_YAY") {
+      DateTime dateTime = dateFormat.parse(data.payload);
+      DafModel todaysDaf = dafsDatesStore.getDafByDate(DateTime(dateTime.year, dateTime.month, dateTime.day));
+      ProgressModel progress = progressAction.get(todaysDaf.masechetId);
+      progress.data[todaysDaf.number] = 1; // TODO: really not ideal.
+      progressAction.update(todaysDaf.masechetId, progress, 5);
+      hiveService.settings.setLastDaf(todaysDaf);
+    }
+  }
+
+  Future onSelectNotification(String payload) async {
   }
 
   @override
@@ -81,18 +149,21 @@ class _WelcomeOnboardingPageState extends State<WelcomeOnboardingPage> {
             padding: EdgeInsets.symmetric(vertical: 48, horizontal: 32),
             children: _listOfLanguages
                 .map((language) => ListTile(
-                      title: ButtonWidget(
-                        text: localizationUtil.translate(
-                            "onboarding", "choose_" + language),
-                        buttonType: ButtonType.Outline,
-                        color: Theme.of(context).primaryColor,
-                        onPressed: () => _changeLanguage(context, language),
-                      ),
-                    ))
+              title: ButtonWidget(
+                text: localizationUtil.translate(
+                    "onboarding", "choose_" + language),
+                buttonType: ButtonType.Outline,
+                color: Theme.of(context).primaryColor,
+                onPressed: () =>  _showNotificationWithActions(),
+              ),
+            ))
                 .toList(),
           )
         ],
       ),
     );
+  }
+
+  Future onDidReceiveLocalNotification(int id, String title, String body, String payload) {
   }
 }
